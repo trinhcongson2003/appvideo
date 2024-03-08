c1a57414820f6ae9f9454a97ad3379ff62e71e78
LUC <luc0363956250@gmail.com>
2024-03-08 22:07:27 +0700

    luc_update

--- app/src/main/java/com/example/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/HomeFragment.java
@@ -18,12 +18,8 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_home, container, false);
-        vd = view.findViewById(R.id.video);
         String videoPath = "android.resource://" + requireContext().getPackageName() + "/" + R.raw.vd;
-        Uri uri = Uri.parse(videoPath);
-        vd.setVideoURI(uri);
-        MediaController mediaController = new MediaController(requireContext());
-        vd.setMediaController(mediaController);
+
 
         return view;
     }
