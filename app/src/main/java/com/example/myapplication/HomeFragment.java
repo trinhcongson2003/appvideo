df2d995b4dc2d94da7df72b6cc2817dea9c7ed57
Khang <ltkhang03@gmail.com>
2024-03-09 14:30:52 +0700

    Khang_update

--- app/src/main/java/com/example/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/HomeFragment.java
@@ -6,20 +6,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.MediaController;
 import android.widget.VideoView;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
-    VideoView vd, vd1;
     View view;
+    ListView listView;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        String videoPath = "android.resource://" + requireContext().getPackageName() + "/" + R.raw.vd;
         view = inflater.inflate(R.layout.fragment_home, container, false);
-        String videoPath = "android.resource://" + requireContext().getPackageName() + "/" + R.raw.vd;
         return view;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
     }
 }
 
